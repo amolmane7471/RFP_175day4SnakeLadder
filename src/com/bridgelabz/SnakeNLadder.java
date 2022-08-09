acb9c7c5efc811982322dcc28342b4bd5e3895ce
amolmane7471 <maneamol3233@gmail.com>
2022-08-09 16:22:17 +0530

    [Amol]:added Snake and Ladder program uc1 for start position 0

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -1,7 +1,16 @@
 package com.bridgelabz;
 public class SnakeNLadder {
+/*
+ * UC1 : snake and ladder game played with single player at start position 0
+ */
+	public void startGame() {
+		int player1 =0;
+		System.out.println("Start Position Of Player1: "+player1);
+		}
+
 public static void main(String[] args) {
 		System.out.println("Welcome To Snake & Ladder");
-
+		SnakeNLadder uc1 = new SnakeNLadder();
+		uc1.startGame();
 	}
 }
