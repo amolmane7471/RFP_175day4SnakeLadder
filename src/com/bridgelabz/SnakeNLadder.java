7783a744a2c9b92c470ea4f05233e7070200350c
amolmane7471 <maneamol3233@gmail.com>
2022-08-09 16:29:25 +0530

    [Amol]:added Snake and Ladder program uc2 to get number between 1 to 6

--- src/com/bridgelabz/SnakeNLadder.java
+++ src/com/bridgelabz/SnakeNLadder.java
@@ -1,4 +1,7 @@
 package com.bridgelabz;
+
+import java.util.Scanner;
+
 public class SnakeNLadder {
 /*
  * UC1 : snake and ladder game played with single player at start position 0
@@ -6,6 +9,14 @@ public class SnakeNLadder {
 	public void startGame() {
 		int player1 =0;
 		System.out.println("Start Position Of Player1: "+player1);
+		Scanner sc = new Scanner(System.in);
+		String str;
+		int diceValue =0;
+		System.out.println("Press r to roll Dice");
+		str = sc.next();
+         diceValue=1+(int)Math.floor(Math.random()*6);
+        System.out.println(" number on dice : " +diceValue);
+
 		}
 
 public static void main(String[] args) {
